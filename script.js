11ee1a7f404a94fdbe3d659f40d6a878b9714c44
Mohd Usman <84888797+aulth@users.noreply.github.com>
2021-08-12 20:15:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// script for dark mode start 
+function $(el){
+    return document.querySelector(el);
+    }
+    let container = $('.container');
+    let toggle = $('#toggle-theme');
+    toggle.addEventListener('click', () =>{
+        let list = container.classList;
+        if(list.contains('default'))
+        {
+            list.replace('default', 'dark');
+            toggle.textContent = 'Light';
+        }
+        else
+        {
+            list.replace('dark', 'default');
+            toggle.textContent = 'Dark'
+        }
+    })
+// script for dark mode ends
